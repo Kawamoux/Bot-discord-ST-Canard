65f3d0764221e206e87d7fb58e3cf461e13320fe
Kawamoux <87915851+Kawamoux@users.noreply.github.com>
2021-07-25 01:25:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-client.on('message', function (message) {
-    if (message.content === '/mc') {
+client.on('message', (message) => {
+    if (message.content === '??mc') {
     message.delete({ timeout: 0000 })
     message.reply('veut jouer à Minecraft ! @everyone')
     }
 })
 
-client.on('message', function (message) {
-    if (message.content === '/pray') {
+client.on('message', (message) => {
+    if (message.content === '??pray') {
     message.delete({ timeout: 0000 })
     message.reply('est entrain de prier le Saint-Canard ! :pray: :pray: :pray:')
     }
