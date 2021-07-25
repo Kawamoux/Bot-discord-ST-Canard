81755d1e5278666e9e4534204f96ebdc4d22bf59
Kawamoux <87915851+Kawamoux@users.noreply.github.com>
2021-07-25 14:03:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ client.on('message', (message) => {
 client.on('message', (message) => {
     if (message.content === '??pray') {
     message.delete({ timeout: 0000 })
-    message.reply('est entrain de prier le Saint-Canard ! :pray: :pray: :pray:')
+    message.reply('est entrain de prier le Saint-Canard !  :pray: :compass: :semiscanard:')
     }
 })
 
