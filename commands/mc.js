6d9d605b772bb42508c798dbda4e558d2a061a43
Kawamoux <87915851+Kawamoux@users.noreply.github.com>
2021-07-25 15:13:46 +0200

    Update mc.js

--- commands/mc.js
+++ commands/mc.js
@@ -1,4 +1,4 @@
-bot.on('message', function (message) {
+client.on('message', function (message) {
     if (message.content === '-mc') {
     message.delete({ timeout: 0000 })
     message.reply('veut jouer à Minecraft ! @everyone')
