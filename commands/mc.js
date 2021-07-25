0c5c2bedb8ae8ade5903dc1843a6bc84669b8f5e
Kawamoux <87915851+Kawamoux@users.noreply.github.com>
2021-07-25 15:16:07 +0200

    Delete mc.js

--- commands/mc.js
+++
@@ -1,6 +0,0 @@
-client.on('message', function (message) {
-    if (message.content === '-mc') {
-    message.delete({ timeout: 0000 })
-    message.reply('veut jouer à Minecraft ! @everyone')
-    }
-})
