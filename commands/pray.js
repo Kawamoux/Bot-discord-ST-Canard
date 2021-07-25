ddfba351e010bf64517d33d573f611173e0a3c35
Kawamoux <87915851+Kawamoux@users.noreply.github.com>
2021-07-25 15:14:05 +0200

    Update pray.js

--- commands/pray.js
+++ commands/pray.js
@@ -1,4 +1,4 @@
-bot.on('message', function (message) {
+client.on('message', function (message) {
     if (message.content === '-pray') {
     message.delete({ timeout: 0000 })
     message.reply('est entrain de prier le Saint-Canard ! :pray: :compass: :semiscanard: ')
