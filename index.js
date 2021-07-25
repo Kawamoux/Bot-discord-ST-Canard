fe37427d3dded052c4fbbc64f67404b387be0bf5
Kawamoux <87915851+Kawamoux@users.noreply.github.com>
2021-07-25 19:28:17 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ client.on('message', (message) => {
     message.channel.send(new Discord.MessageEmbed()
     .setTitle('Tout sur le Bot ! :duck:')
     .setColor('#FFD700')
-    .addField('Commandes :\n', '??help : Pour tout savoir\n??pray : Pour prier le Saint-Canard\n??mc : Pour jouer à Minecraft\n??coink : Pour savoir le bruit que fait le cannard'))
+    .addField(`Commandes :
+    `, '??help : Pour tout savoir\n??pray : Pour prier le Saint-Canard\n??mc : Pour jouer à Minecraft\n??coink : Pour savoir le bruit que fait le cannard'))
     }
 })
 
