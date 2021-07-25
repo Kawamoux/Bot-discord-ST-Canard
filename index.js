2f73e415b6998f8a469786b73c7197c89a3789ca
Kawamoux <87915851+Kawamoux@users.noreply.github.com>
2021-07-25 19:13:47 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ client.on('message', (message) => {
     if (message.content === '??help') {
     message.delete({ timeout: 0000 })
     message.channel.send(new Discord.MessageEmbed()
-    .setTitle('Tout sur le Bot ! :duck:'))
-    .setDescription()
+    .setTitle('Tout sur le Bot ! :duck:')
+    .addField('Commandes :', '??help : Pour tout savoir\n??pray : Pour prier le Saint-Canard\n??mc : Pour jouer à Minecraft\n??coink : Pour savoir le bruit que fait le cannard')
+    .setFooter('Fait par Kawamo\n[Lien du Code GITHUB](https://github.com/Kawamoux/Bot-discord-ST-Canard)'))
     }
 })
 
