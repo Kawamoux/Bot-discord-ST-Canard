660b76795b7aa34a83d6108a53e768cdea27bd69
Kawamoux <87915851+Kawamoux@users.noreply.github.com>
2021-07-25 15:17:37 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.on('message', (message) => {
+    if (message.content === '??mc') {
+    message.delete({ timeout: 0000 })
+    message.reply('veut jouer à Minecraft ! @everyone')
+    }
+})
+
+client.on('message', (message) => {
+    if (message.content === '??pray') {
+    message.delete({ timeout: 0000 })
+    message.reply('est entrain de prier le Saint-Canard !  :pray: :compass: :duck:')
+    }
+})
+
+client.login(process.env.TOKEN);
