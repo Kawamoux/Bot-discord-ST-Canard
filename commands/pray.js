3f53e9241ae39ac80343bb9255b94041684dfbc7
Kawamoux <87915851+Kawamoux@users.noreply.github.com>
2021-07-25 15:16:16 +0200

    Delete pray.js

--- commands/pray.js
+++
@@ -1,6 +0,0 @@
-client.on('message', function (message) {
-    if (message.content === '-pray') {
-    message.delete({ timeout: 0000 })
-    message.reply('est entrain de prier le Saint-Canard ! :pray: :compass: :semiscanard: ')
-    }
-})
