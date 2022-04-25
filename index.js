04f4b37451f3b52b7dc1fe01a6ff81104245bf2f
Kawamoux <87915851+Kawamoux@users.noreply.github.com>
2022-04-25 19:05:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+client.user.setActivity("J'attends toujours la VF de Ratatoing...");
 
 client.on('message', (message) => {
     if (message.content === '??help') {
